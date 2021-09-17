4db10844a5e67bd5600069174144019867c9e1ac
= <enricovivan21@gmail.com>
2021-09-16 21:04:12 -0300

    persistência local

--- lib/app/view/homepage.dart
+++ lib/app/view/homepage.dart
@@ -9,26 +9,64 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+final listaPedidos = [
+  {
+    'nomePrato': 'Steak Tartare',
+    'quantia': '2 Pratos',
+    'fotoPrato':
+        'https://www.lowcarbnikki.com/wp-content/uploads/2019/08/20190611_203100.jpg'
+  },
+  {
+    'nomePrato': 'Pidan',
+    'quantia': '4 Porções',
+    'fotoPrato':
+        'https://www.onlinepasar.com.my/wp-content/uploads/2020/02/Century-Egg-Pidan.jpg'
+  },
+  {
+    'nomePrato': 'Pato no Tucupi',
+    'quantia': '2 Pratos',
+    'fotoPrato':
+        'https://t2.rg.ltmcdn.com/pt/images/4/1/9/img_pato_no_tucupi_paraense_2914_orig.jpg'
+  },
+];
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(MyApp.CRONOMETRO_GENERAL);
-              },
-              icon: Icon(Icons.alarm)),
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(MyApp.PEDIDOS_FORM);
-              },
-              icon: Icon(Icons.add)),
-        ],
-      ),
-      body: ListView(),
-    );
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(MyApp.CRONOMETRO_GENERAL);
+                },
+                icon: Icon(Icons.alarm)),
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(MyApp.PEDIDOS_FORM);
+                },
+                icon: Icon(Icons.add)),
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: listaPedidos.length,
+            itemBuilder: (context, i) {
+              var prato = listaPedidos[i];
+              var foto = CircleAvatar(
+                  backgroundImage: NetworkImage(prato['fotoPrato']!));
+              return ListTile(
+                leading: foto,
+                title: Text(prato['nomePrato']!),
+                subtitle: Text(prato['quantia']!),
+                trailing: Container(
+                  width: 100,
+                  child: Row(children: [
+                    IconButton(onPressed: null, icon: Icon(Icons.cancel)),
+                    IconButton(onPressed: null, icon: Icon(Icons.timer)),
+                  ]),
+                ),
+              );
+            }));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'app/my_app.dart';
 
-void main() {
+void main() async {
+  await loadInit();
+  await loadInterface();
+  await loadButtons();
+  print('Executando...');
   runApp(const MyApp());
+}
+
+// funções assíncronas
+Future loadInit() =>
+    Future.delayed(Duration(seconds: 2), () => print('Inicializando o APP...'));
+
+Future loadInterface() {
+  return Future.delayed(
+      Duration(seconds: 2), () => print('Carregando Interface...'));
+}
+
+Future loadButtons() {
+  return Future.delayed(Duration(seconds: 2), () => print('Carregando botões'));
 }
