535b6dd308431929c4faba9665efe10276bd988e
= <enricovivan21@gmail.com>
2021-10-29 11:48:34 -0300

    firestore_default

--- lib/app/injection.dart
+++ lib/app/injection.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:projeto_bom_joia/app/database/firestore/pratos_dao_firestore.dart';
 import 'package:projeto_bom_joia/app/database/sqlite/dao/pratos_dao_impl.dart';
+import 'package:projeto_bom_joia/app/domain/entities/pratos.dart';
 import 'package:projeto_bom_joia/app/domain/interfaces/pratos_dao.dart';
 import 'package:projeto_bom_joia/app/domain/services/prato_service.dart';
 
@@ -15,6 +16,6 @@ configuraInjecao() async {
   await Firebase.initializeApp();
   //registerSingleton cria um unico objeto pra atender todo mundo // compartilha as informações do objeto
   //registerFactory cria um objeto para cada requisição
-  getIt.registerSingleton<PratosDAO>(PratosDAOImpl());
+  getIt.registerSingleton<PratosDAO>(PratosDAOFirestore());
   getIt.registerSingleton<PratoService>(PratoService());
 }
